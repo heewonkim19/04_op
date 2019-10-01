2562c3b4444ca257f3cb6978d66248a4a848ad27
heewonkim19 <55428337+heewonkim19@users.noreply.github.com>
2019-10-01 09:41:57 +0900

    03

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/* project3 */
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
